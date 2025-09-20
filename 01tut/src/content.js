9ab718cea43b0a00f1bf072e7a9b2510fcf83392
Pragyan <pragyanlohani022@gmail.com>
2025-09-21 00:15:55 +0530

    new-content.js Test

--- 01tut/src/content.js
+++ 01tut/src/content.js
@@ -1,43 +1,74 @@
 
-import { use, useState } from "react";
+import {useState } from "react";
 const Content = () =>{
 
-    const [name, setName] =  useState("pragyan");
+         const [item,setitem] = useState([
+        {
+            id: 1,
+            checked: true,
+            item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item 2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        }
+    ]);
 
-    const [count,setCount] = useState(0);
-
-      const handelNameChange = () => {
-    const names = ["amit" ,"dave " ,"ankit"];
-    const int = Math.floor(Math.random()*3);
-     setName(names[int]);
-
-  }
 
 
-  const handelClick = () =>{
-    setCount (count+1);
-    console.log(count);
-  };
 
-  const handelClickTwo = (name) =>{
-    console.log(`${name}`)
-  };
-  const handelClickThree = (e) =>{
-    console.log(e)
-  };
+    // const [name, setName] =  useState("pragyan");
+
+    // const [count,setCount] = useState(0);
+
+  //     const handelNameChange = () => {
+  //   const names = ["amit" ,"dave " ,"ankit"];
+  //   const int = Math.floor(Math.random()*3);
+  //    setName(names[int]);
+
+  // }
+
+
+  // const handelClick = () =>{
+  //   setCount (count+1);
+  //   console.log(count);
+  // };
+
+  // const handelClickTwo = (name) =>{
+  //   console.log(`${name}`)
+  // };
+  // const handelClickThree = (e) =>{
+  //   console.log(e)
+  // };
 
 return (
 
     <main>
-      
-      <p>
+        <ul>
+          {items.map((items) => (
+            <li className="item"></li>
+
+
+          ))}
+        </ul>
+
+
+      {/* <p>
         hello {name}!
       </p>
       <button onClick={handelNameChange}>change name</button>
       <button onClick= {handelClick }>Count</button>
-      <button onClick= {(e) =>handelClickThree(e)}>Click</button>
+      <button onClick= {(e) =>handelClickThree(e)}>Click</button> */}
 
     </main>
+
+    
 )
 
 };
